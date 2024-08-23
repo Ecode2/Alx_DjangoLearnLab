bc156eb493594490d7bf43963b83e7f9f5f858df
Ecode2 <ecode5814@gmail.com>
2024-08-23 21:55:00 +0100

    nievdfcf

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -52,5 +52,5 @@ def save_user_profile(sender, instance, **kwargs):
 
 class Meta:
     "permissions"
-
+    "can_add_book", "can_change_book", "can_delete_book"
     "can_add_book", "can_change_book", "can_delete_book"
