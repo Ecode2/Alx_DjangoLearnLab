4ed8df237354be7b07861d9e0a61351160d6a292
Ecode2 <ecode5814@gmail.com>
2024-08-23 21:44:24 +0100

    modified models for custom admin

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -26,8 +30,25 @@ class Librarian(models.Model):
 
 
 class UserProfile(models.Model):
-    role = models.CharField(max_length=100, default="Admin",) 
-    "Member"
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 class Meta:
     "permissions"
