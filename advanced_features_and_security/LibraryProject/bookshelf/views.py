1471088f184a5cce1be90e0bc190fad1f7b92660
Ecode2 <ecode5814@gmail.com>
2024-08-25 22:40:40 +0100

    lrefjcdx

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required, permission_required
-
+from .models import Book
 # Create your views here.
 
 @permission_required('app_name.can_edit', raise_exception=True)
 def my_view(request):
     # Your view logic here
-    pass
+    book_list = Book.objects.all()
+
+    if not book_list:
+        raise ValueError('raise_exception books')
 
 """ @login_required
 @permission_required('app_name.can_edit', raise_exception=True)
