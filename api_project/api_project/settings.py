da4c4a27c817a2d5927bcc7867b12694587691a4
Ecode2 <ecode5814@gmail.com>
2024-09-01 23:59:16 +0100

    retrvdcs

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_AUTHENTICATION_CLASSES="rest_framework.authentication.TokenAuthentication"
+
+"rest_framework.permissions.IsAuthenticated"
