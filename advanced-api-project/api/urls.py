a6743cd38a1a4aaa5f247779796f180300a47bd6
Ecode2 <ecode5814@gmail.com>
2024-09-08 21:55:51 +0100

    finished filtering

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path("/books/", ListView.as_view()),
-    path("/books/delete/<int:pk>/", DeleteView.as_view()),
-    path("/books/create/<int:pk>/", CreateView.as_view()),
-    path("/books/<int:pk>/", DetailView.as_view()),
-    path("/books/update/<int:pk>/", UpdateView.as_view()),
+    path("books/", ListView.as_view()),
+    path("books/delete/<int:pk>/", DeleteView.as_view()),
+    path("books/create/", CreateView.as_view()),
+    path("books/<int:pk>/", DetailView.as_view()),
+    path("books/update/<int:pk>/", UpdateView.as_view()),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
+#from django_filters import rest_framework
 
 from .serializers import BookSerializer
 from .models import Book
 
 # Create your views here.
 class ListView(generics.ListAPIView):
-    queryset=Book.objects.all()
     serializer_class=BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fileds = ["title", "publication_year"]
+    ordering_fileds = ["title", "publication_year"]
     
     def get_queryset(self):
+        """ author = self.kwargs['author']
+        return Book.objects.filter(author=author) """
+        queryset = Book.objects.all()
         author = self.request.query_params.get('author')
         if author is not None:
-            queryset = queryset.filter(author.name=author)
-            return queryset
+            queryset = queryset.filter(author=author)
         return queryset
+    
+    #filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
 
 class DetailView(generics.RetrieveAPIView):
