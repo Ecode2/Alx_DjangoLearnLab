6edaadf1a0a565f36ed725afed7ca2feb6a7b543
Ecode2 <ecode5814@gmail.com>
2024-09-08 20:48:23 +0100

    fixed urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path("books/", ListView.as_view()),
-    path("books/<int:pk>/", DeleteView.as_view()),
-    path("books/<int:pk>/", CreateView.as_view()),
-    path("books/<int:pk>/", DetailView.as_view()),
-    path("books/<int:pk>/", UpdateView.as_view()),
+    path("/books/", ListView.as_view()),
+    path("/books/<int:pk>/", DeleteView.as_view()),
+    path("/books/<int:pk>/", CreateView.as_view()),
+    path("/books/<int:pk>/", DetailView.as_view()),
+    path("/books/<int:pk>/", UpdateView.as_view()),
 ]
