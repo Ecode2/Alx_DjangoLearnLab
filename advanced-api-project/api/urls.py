ed9fe72bceb6166582f5f405b5b6187113492dc7
Ecode2 <ecode5814@gmail.com>
2024-09-08 20:50:23 +0100

    fixed urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,8 +4,8 @@ from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path("/books/", ListView.as_view()),
-    path("/books/<int:pk>/", DeleteView.as_view()),
-    path("/books/<int:pk>/", CreateView.as_view()),
+    path("/books/delete/<int:pk>/", DeleteView.as_view()),
+    path("/books/create/<int:pk>/", CreateView.as_view()),
     path("/books/<int:pk>/", DetailView.as_view()),
-    path("/books/<int:pk>/", UpdateView.as_view()),
+    path("/books/update/<int:pk>/", UpdateView.as_view()),
 ]
