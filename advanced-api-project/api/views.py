04a0c0b2a146e4884ff2c95b899af503aed00600
Abubakar Alaya <ecode5814@gmail.com>
2024-09-08 21:07:07 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 
 from .serializers import BookSerializer
 from .models import Book
@@ -10,6 +11,14 @@ class ListView(generics.ListAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_queryset(self):
+        author = self.request.query_params.get('author')
+        if author is not None:
+            queryset = queryset.filter(author.name=author)
+            return queryset
+        return queryset
+
 
 class DetailView(generics.RetrieveAPIView):
     queryset=Book.objects.all()
