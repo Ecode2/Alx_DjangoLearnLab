1d6592c9bd2ebe526a52ede3b8a5382d000f95b9
Ecode2 <ecode5814@gmail.com>
2024-09-13 12:35:34 +0100

    made changes

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import Post
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class UserUpdateForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,19 @@
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from django.views.generic import ListView, DetailView, TemplateView, CreateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import logout
 
-from .forms import PostForm
+from .forms import PostForm, UserUpdateForm
 from .models import Post
 
 # Create your views here.
@@ -29,15 +32,32 @@ class CustomLogoutView(LogoutView):
         logout(request)
         return redirect("blog:home")
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, UpdateView, DetailView):
+    model= User
+    form_class= UserUpdateForm
     template_name="blog/profile.html"
+    success_url = reverse_lazy("blog:profile")
+
+    "POST", "save()"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-        context["user"]=user
-        context["password"]=user.password
+        context["form"]=self.get_form()
         return context
+    
+    def get_object(self):
+        return self.request.user
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+
 
 class HomeView(TemplateView):
     template_name = "blog/home.html"
@@ -77,3 +97,8 @@ class CreatePost(CreateView, LoginRequiredMixin):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+    
+class UpdatePost(UpdateView, LoginRequiredMixin):
+        model = Post
+        template_name = "blog/create_post.html"
+    
