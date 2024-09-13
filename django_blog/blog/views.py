7d36f3837cf03b97c21dc30b0d779679c34b61f0
Ecode2 <ecode5814@gmail.com>
2024-09-13 12:36:54 +0100

    fixed signup template

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -20,7 +20,7 @@ from .models import Post
 class SignupView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('blog:login')
-    template_name = "blog/signup.html"
+    template_name = "blog/register.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {"form": self.form_class})
