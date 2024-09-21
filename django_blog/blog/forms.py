66170a7f140254fa52f58492b531e1b828583f13
Ecode2 <ecode5814@gmail.com>
2024-09-21 16:04:31 +0100

    ervinkweo

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Post, Comment
 
+# TagWidget()", "widgets.SelectMultiple(attrs={"class": "form-control"})
 class PostForm(ModelForm):
     class Meta:
         model = Post
