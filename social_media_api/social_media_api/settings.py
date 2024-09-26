6401095ca463923667ffffe9e97889b4d8765a4c
Ecode2 <ecode5814@gmail.com>
2024-09-26 23:14:48 +0100

    changed allowed host

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["social-media-api-73x2.onrender.com"]
 
 SECURE_SSL_REDIRECT=True
 
